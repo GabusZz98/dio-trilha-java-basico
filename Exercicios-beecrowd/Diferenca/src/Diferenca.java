c0c21b77e8fd22a7e6d0832a832778e65394b58f
GabusZz98 <gabrielhsoares2013@gmail.com>
2024-08-29 08:42:38 -0300

    Exercicio de Diferenca Resolvido

--- Exercicios-beecrowd/Diferenca/src/Diferenca.java
+++ Exercicios-beecrowd/Diferenca/src/Diferenca.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Diferenca {
     public static void main(String[] args) throws Exception {
         
-        int A, B, C, D;
+        Scanner scanner = new Scanner(System.in);
+
+        int A, B, C, D, diferenca;
+
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = scanner.nextInt();
+        D = scanner.nextInt();
+
+        diferenca = (A * B - C * D);
+
+        System.out.println("DIFERENCA = " + diferenca);
         
+        scanner.close();
     }
 }
